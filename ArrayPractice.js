7604b8fd4bb1f80aeccae7314ac8b0ae0b369d77
Anish Dey <adanishdey59@gmail.com>
2023-03-14 02:10:57 +0530

    New upload

---
+++ ArrayPractice.js
@@ -0,0 +1,21 @@
+// Move all negative numbers to beginning and positive to end with constant extra space
+
+function arrangeNegatives(arr){
+    let n = arr.length;
+    let j = 0;
+    for(let i = 0; i < n; i++){
+        if(arr[i] < 0 && i!=j){
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+            j++;
+        }
+    }
+    return arr;
+}
+
+
+
+const arr = [-12, 11, -13, -5, 6, -7, 5, -3, -6]
+console.log(arr)
+console.log("After arranging")
+console.log(arrangeNegatives(arr))
+
